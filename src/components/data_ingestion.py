1f3b7be3b9fdc7c580ffa86a0e9306bf3e227516
NirmalVignu <nirmalvignu@gmail.com>
2023-04-15 19:04:30 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -41,8 +41,3 @@ class DataIngestion:
             logging.info('Exception occured at Data Ingetion stage')
             raise CustomException(e,sys)
         
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_trainsformation=DataTransformation()
-    train_arr,test_arr,_=data_trainsformation.initiate_data_transformation(train_data_path,test_data_path)
